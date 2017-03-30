FIX THE BUGS IN RNG FOR PARALLEL CALCULATION

SOURCE/doplot.py
@@ -37,9 +37,15 @@ def sp(geo,res):
 
 def reso(ini,res):
     fig = plt.figure()
-    levels = np.linspace(0,1,res.segs)
-    cs = plt.contour(ini.r,ini.z,np.sqrt(np.transpose(ini.psirz)),levels,vmin=0,vmax=1)
-    cb = plt.colorbar(cs, shrink=0.8, extend='both',ticks=levels[0::2])
+    levels=[0,0.2,0.4,0.6,0.8,1.0]
+    rho = res.rho
+    rhomin = res.rhomin
+    rhomax = res.rhomax
+
+    sc = plt.contour(rhomin[1][0:100],rhomin[2][0:100],rhomin[0].T,levels=levels,linewidth=40)
+    sc = plt.contour(rho[1][0:100],rho[2][0:100],rho[0].T,levels=levels,linewidth=40)
+    sc = plt.contour(rhomax[1][0:100],rhomax[2][0:100],rhomax[0].T,levels=levels,linewidth=4)
+    cb=plt.colorbar(sc)
 
 
     plt.clabel(cs, levels[0::2], inline=1, fontsize=10)

SOURCE/grid_gen.py
@@ -12,48 +12,48 @@ def main():
    geometry = np.load('./output/geometry.npz')
    foil = geometry['foil']
    pinhole = geometry['pinhole']
-   
+
    output = np.load('./output/inpa_grid.npz')
    sp = output['strike_pos']
    e_ini = output['E_ini']
    r_ini = output['R_ini']
-   
+
    num = 100000
    point = np.zeros((num,2))
    x = sp[:,1]/np.cos(15/180*np.pi)
    z = sp[:,2]/np.cos(15/180*np.pi)
-   
+
    xfit = np.linspace(0.35,0.6,100)
-   
+
    R = np.arange(2.2,1.4,-0.1)
    E = np.arange(20,80,10)
    fit = np.zeros((len(R),2))
    grid_R = np.zeros((len(R),len(xfit)))
    grid_E = np.zeros((len(E),len(xfit)))
-   
+
    for i in np.arange(len(R)):
        mask = np.where(((r_ini[:,0]>R[i]-0.002) & (r_ini[:,0]<R[i]+0.002)))
        fit[i,:] = np.polyfit(x[mask],z[mask],1)
        fit_fn = np.poly1d(fit[i,:])
        grid_R[i,:] = fit_fn(xfit)
-   
+
    fit = np.zeros((len(E),6))
    for i in np.arange(len(E)):
        mask = np.where(((e_ini>E[i]-1) & (e_ini<E[i]+1)))
        fit[i,:] = np.polyfit(x[mask],z[mask],5)
        fit_fn = np.poly1d(fit[i,:])
        grid_E[i,:] = fit_fn(xfit)
-   
+
 #   fig = plt.figure(1)
 #   fig.clf()
 #   for i in np.arange(len(R)):
 #       plt.plot(xfit,grid_R[i,:],color='black')
 #   for i in np.arange(len(E)):
 #       plt.plot(xfit,grid_E[i,:],color='black')
-#   
-   
+#
+
    return xfit,grid_R,  grid_E, R,E
-    
+
 
 
 

SOURCE/inpa_odepara.py
@@ -1,46 +1,46 @@
 """
-;+
-; NAME:
-;     INPA SIMULATION
-; PURPOSE:
-      This Monte Carlo code calculates the orbit from carbon foil to the scintillator
-; EXPLANATION:
-      The neutral are randomly generated from plasma following the rules:
-      (1) The energy of the neutral is randomly generated from 0keV to 80keV
-      (2) The direction of neutral flying is randomly vector between one point
-	  in aperture and the other in carbon foil.
-      (3) The initial velocity [vx,vy,vz] is then determined.
-      (4) The larmor motion of the particle is then traced until it hits
-          the scintillator, i.e.,vz(t0)*vz(t0+dt)<0.
-;
-; CALLING SEQUENCE:
-        def geometry() in design.py is depenedent file
-#;
-; OPTIONAL INPUT:
-;
-; OPTIONAL KEYWORD INPUT:
-;
-; OPTIONAL KEYWORD OUTPUTS:
++
+ NAME:
+     INPA SIMULATION
+ PURPOSE:
+     This Monte Carlo code calculates the orbit from carbon foil to the scintillator
+ EXPLANATION:
+     The neutral are randomly generated from plasma following the rules:
+     (1) The energy of the neutral is randomly generated from 0keV to 80keV
+     (2) The direction of neutral flying is randomly vector between one point
+     in aperture and the other in carbon foil.
+     (3) The initial velocity [vx,vy,vz] is then determined.
+     (4) The larmor motion of the particle is then traced until it hits
+         the scintillator, i.e.,vz(t0)*vz(t0+dt)<0.
 
-; PROCEDURE:
+ CALLING SEQUENCE:
+       def geometry() in design.py is depenedent file
 
-; EXAMPLE:
-       run inpa.py
-; RESTRICTIONS:
-;
-; PROCEDURES USED:
-;
-; REVISION HISTORY:
-      first usable version is inpa_v2.py
-		by DU,Xiaodi on 08-09-2016
+ OPTIONAL INPUT:
 
-      change the design function purely 3D
-	     design.geometry_3d(la, lb, lc)
-		by DU,Xiaodi on 08-16-2016
+ OPTIONAL KEYWORD INPUT:
 
-      add a routine to generate the input file of NPA GEOMETRY for FIDASIM
-	     output4_fidasim.input(p1,p2,p3,p4,f1,f2,f3,f4,w,h)
-		by DU,Xiaodi on 11-3-2016
+ OPTIONAL KEYWORD OUTPUTS:
+
+ PROCEDURE:
+
+ EXAMPLE:
+      run inpa.py
+ RESTRICTIONS:
+
+ PROCEDURES USED:
+
+ REVISION HISTORY:
+     first usable version is inpa_v2.py
+   	by DU,Xiaodi on 08-09-2016
+
+     change the design function purely 3D
+        design.geometry_3d(la, lb, lc)
+   	by DU,Xiaodi on 08-16-2016
+
+     add a routine to generate the input file of NPA GEOMETRY for FIDASIM
+        output4_fidasim.input(p1,p2,p3,p4,f1,f2,f3,f4,w,h)
+   	by DU,Xiaodi on 11-3-2016
 
 """
 import sys
@@ -57,7 +57,6 @@ import read_hd5
 import particle_launcher
 import numpy as np
 from scipy.integrate import odeint
-from joblib import Parallel, delayed
 
 '''particle location at t+dt'''
 def track(s_t0, v, tstep):
@@ -88,7 +87,7 @@ def random_3d(p0,p1):
 
 
 '''initial position on scintillator'''
-def initial_pos(geo,ini):
+def initials(geo,ini):
 
     p1 = geo.pinhole[0,:]
     p2 = geo.pinhole[1,:]
@@ -123,17 +122,6 @@ def initial_pos(geo,ini):
     bt = bfield.brzt(lr,lz,lphi,ini)
 
     return n0_p, n0_f, bt, r_loc, pitch_loc
-
-'''
-    initial energy of neutrals
-'''
-def initial_E(ini):
-    # initial energy [keV]
-    Eth  = ini.Emin
-    Enbi = ini.Emax
-    E_energy =Eth + (Enbi-Eth)*np.random.random(1) #keV
-
-    return E_energy
 
 
 '''initial velocity of neutrals'''
@@ -185,29 +173,29 @@ def ode_equ(s,t,fr,fz,rc,bc,charge,mass):
 
     return np.array([vx,vy,vz,ax,ay,az])
 
-def dummy_input(ini,geo):
-    return ini,geo
+# -- RANDOM NUMBER GENERATOR FOR PARALLEL CALCULATION --
+def RNG(ini,geo):
+    ini.n0_p = np.zeros((ini.mc,3))
+    ini.n0_f = np.zeros((ini.mc,3))
+    ini.B_birth = np.zeros((ini.mc,3))
+    ini.R_birth = np.zeros((ini.mc,2))
+    ini.P_birth = np.zeros(ini.mc)
+    ini.E_birth = np.random.uniform(ini.Emin,ini.Emax,size=ini.mc)
 
-def PARA_mc(i):
-    #ini,geo = dummy_input(ini,geo)
-    # initial energy
-    E_ini = initial_E(ini)
+    for i in range(0,ini.mc):
+        ini.n0_p[i,:], ini.n0_f[i,:], ini.B_birth[i,:], ini.R_birth[i], ini.P_birth[i] \
+         = initials(geo,ini)
+    return ini
 
-    # generate initail values for orbit tracing
-    n0_p, n0_f, bt, R_ini, P_ini \
- 	 = initial_pos(geo,ini)
+# -- PARALLEL CALCULATION -- #
+def PARA_mc(ini,geo,i):
+
+    # initail conditions for orbit tracing
+    n0_p, n0_f, E_ini \
+ 	= ini.n0_p[i,:], ini.n0_f[i,:], ini.E_birth[i]
 
     # initial velocity in xyz coordinate
     v_ini = initial_v(E_ini,n0_p,n0_f)
-
-    # birth place at the pinhole and foil
-    #birth_sl[mc,0,:] = np.array(n0_p)
-    #birth_sl[mc,1,:] = np.array(n0_f)
-
-    # birth place of R, pitch, and Energy
-    #R_ini[mc,:] = r_loc
-    #P_ini[mc] = pitch_loc
-    #E_ini[mc] = E_energy
 
     # -----------------------
     #     solve ode         |
@@ -216,11 +204,13 @@ def PARA_mc(i):
     ini_y0 = np.concatenate((n0_f,v_ini))
 
     # time slices for integrator
+    tstep = np.copy(ini.tstep)
+    steps = np.copy(ini.steps)
     tsol = np.arange(0,tstep*steps,tstep)
 
     #ODE INTEGRATOR
     sol = odeint(ode_equ,ini_y0,tsol,
-                 args=(ini.fr,ini.fz,ini.equ['rcentr'],ini.equ['bcentr'],ini.charge,ini.mass))
+          args=(ini.fr,ini.fz,ini.equ['rcentr'],ini.equ['bcentr'],ini.charge,ini.mass))
 
     # determine hitting points
     tmplower =  np.sign(
@@ -237,82 +227,5 @@ def PARA_mc(i):
        #   geo.uphoreq[2]*sol[:,2] +
        #   geo.uphoreq[3]
        #   )
-    return R_ini, P_ini, E_ini, hit_point
-
-'''--------------'''
-'''MAIN Program'''
-'''--------------'''
-
-# -- test various geometry --
-import geometry
-import geometry_failed3
-import geometry_upgrade
-import geometry_upgrade2
-import geometry_upgrade3
-
-fpath = '/home/duxiaodi/inpa/GFILE/159243C04_801.geq'
-#  -- INPUT --     mc,  tstep, steps, Eini, Emax, gfile
-ini = creatobj.ini(5000, 1e-11, 10000,  20,  80,  fpath)
-ini.charge = 1.6*(1e-19) # D charge
-ini.mass = 2*1.67*(1e-27)# D mass
-
-# -- EQUILIBRIUM --
-ini.fr,ini.fz,ini.equ = bfield.prepare(ini.gfile)
-
-# -- INPA GEOMETRY --
-geo = geometry_failed3.main()
-
-# -- CALL MAIN --
-ini.cores = 10
-
-
-# --- INPUT ZONE ---
-# number of monte carlo particles
-ini.mc = ini.mc
-tstep = ini.tstep
-steps = ini.steps
-
-# inital R position
-R_ini = np.zeros((ini.mc,2))
-
-# inital pitch
-P_ini = np.zeros(ini.mc)
-
-# inital energy
-E_ini = np.zeros(ini.mc)
-
-# record everthing in history of one particle
-s_hist = np.zeros((steps,3))
-s_histx = np.zeros((ini.mc,steps,3))
-a_hist = np.zeros((steps,3))
-v_hist = np.zeros((steps,3))
-bt_hist = np.zeros((steps,3))
-
-# birth sightline
-birth_sl = np.zeros((ini.mc,2,3))
-
-# position of striking points
-strike_pos = np.zeros((ini.mc,3))
-
-# light emission on scintillator
-light = np.zeros(ini.mc)
-
-if __name__ == '__main__':
-   ini,geo = dummy_input(ini,geo)
-   #R_ini[i,:],P_ini[i],E_ini[i],strike_pos[i,:] = \
-   output = Parallel(n_jobs=ini.cores)(delayed(PARA_mc)(i) \
-            for i in range(0,ini.mc) \
-            )
-   for i in range(0,ini.mc):
-       R_ini[i,:] = output[i][0]
-       P_ini[i] = output[i][1]
-       E_ini[i] = output[i][2]
-       strike_pos[i,:] = output[i][3]
-
-# structure for output
-res = creatobj.result(R_ini,-1.0*np.cos(P_ini/180.*np.pi),E_ini)
-res.hitpoint = strike_pos
-res.birthsl = birth_sl
-
-
+    return hit_point
 

SOURCE/resolution.py
@@ -21,7 +21,10 @@ def main(ini,res):
 
     # calculate local psi
     psi_seg = np.zeros((ini.mc,segs))
+
     # rho resolution
+    rho = np.zeros((ini.mc,segs))
+    rhoavg = np.zeros(ini.mc)
     drho = np.zeros(ini.mc)
 
     # pitch resolution
@@ -33,21 +36,28 @@ def main(ini,res):
     vb = np.zeros((ini.mc,segs,3))
     # ??
     pitch = np.zeros((ini.mc,segs))
+    pitchavg = np.zeros(ini.mc)
 
     for i in np.arange(ini.mc):
         for j in np.arange(segs):
             psi_seg[i,j] = ini.fpsi(rz_seg[i,j,1],rz_seg[i,j,0])
+            rho[i,j] = np.sqrt(psi_seg[i,j])
             vb[i,j,:] = bfield.brzt(rz_seg[i,j,0],rz_seg[i,j,1],rz_seg[i,j,2],ini)
             pitch[i,j] = lib.angle_between(vb[i,j,:], res.birthsl[i,1,:]-res.birthsl[i,0,:])
 
-    R_mid_avg[i] = np.mean(rz_seg[i,:,0])
-    drho[i] = np.amax(np.sqrt(psi_seg[i,:]))-np.amin(np.sqrt(psi_seg[i,:]))
-    dpitch[i] = np.amax(pitch[i,:]) - np.amin(pitch[i,:])
+        R_mid_avg[i] = np.mean(rz_seg[i,:,0])
+        rhoavg[i] = np.mean(rho[i,:])
+        pitchavg[i] = np.mean(pitch[i,:])
+        drho[i] = np.amax(np.sqrt(psi_seg[i,:]))-np.amin(np.sqrt(psi_seg[i,:]))
+        dpitch[i] = np.amax(pitch[i,:]) - np.amin(pitch[i,:])
 
+    res.rho = rhoavg
     res.rho_eb = drho
+
+    res.pitch = pitchavg
     res.pitch_eb = drho
+
     res.rzseg = rz_seg
-    res.pitch = pitch
     res.segs = segs
     return res
 

SOURCE/specif.py
@@ -25,7 +25,8 @@ def main(p0,p1,ini):
    b_loc = bfield.brzt(lr,lz,lphi,ini)
 
    # pitch angle between sightline and magnetic field
-   pitch_loc = lib.angle_between(p1-p0,b_loc)/np.pi*180.
+   pitch_loc = lib.angle_between(p1-p0,b_loc)
+
 
    return np.asarray([lr,lz]), pitch_loc
 